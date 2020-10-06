27b82e90afc38435fccd90af56b60140a47ecff9
Qing Ran <qingr@WICRESOFT.COM>
2020-10-06 22:29:41 +0800

    first commit

---
+++ HttpTrigger1/__init__.py
@@ -0,0 +1,42 @@
+import logging
+import paramiko
+from paramiko import AutoAddPolicy
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    ##added
+    ##connecting to VM
+    hostname = '104.43.169.106' # VIP = '10.3.1.4'
+    port = 22
+    username = 'yuran'
+    password = 'Yuukochan!1979'
+
+    
+    s = paramiko.SSHClient()
+    s.load_system_host_keys()
+    s.set_missing_host_key_policy(AutoAddPolicy())
+    s.connect(hostname, port, username, password)
+    command = 'python3 hello.py'
+    (stdin, stdout, stderr) = s.exec_command(command)
+    for line in stdout.readlines():
+        print(line)
+    s.close()
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
